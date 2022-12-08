8dc8c7b33727cb03dce003b61270edbf35199702
GyanSut <sarfosuttah@gmail.com>
2022-12-08 12:06:26 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
-#include <stdio.h>                                                                                                                                                                       
-                                                                                                                                                                               
+#include <stdio.h>
+
 /**
  * main - Entry Point
  *
  * Return: Always 0 (Success)
  */
-                                                                                                                                                                                         
-int main(void)                                                                                                                                                                           
-{                                                                                                                                                                                        
-	        printf("\"Programming is like building a multilingual puzzle");                                                                                                                    
-		return (0);                                                                                                                                                                      
-			                                                                                                                                                                                         
-}    
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
